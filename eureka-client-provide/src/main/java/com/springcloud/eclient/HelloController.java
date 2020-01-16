fe1a0fa5520714ae0a11e9f82119ff7da7001b08
840369583@qq.com <840369583@qq.com>
2020-01-16 18:03:36 +0800

    代码提交

--- eureka-client-provide/src/main/java/com/springcloud/eclient/HelloController.java
+++ eureka-client-provide/src/main/java/com/springcloud/eclient/HelloController.java
@@ -1,6 +1,7 @@
 package com.springcloud.eclient;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,11 +9,13 @@ public class HelloController {
     
     @GetMapping("/hello")
     public String hello() {
+	System.out.println("hello world!");
         return "hello world!";
     }
     
-    @GetMapping("/say")
-    public String say(String name) {
+    @GetMapping("/say/{name}")
+    public String say(@PathVariable String name) {
+	System.out.println("hello "+name);
         return "hello "+name;
     }
 }
