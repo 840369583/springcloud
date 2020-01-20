d7614e13b31077ac11465c98afe15b606bf83891
840369583@qq.com <840369583@qq.com>
2020-01-20 18:31:20 +0800

    feign+hystrix 健康监测

---
+++ eureka-client-feign-hystrixstream/src/main/java/com/springcloud/feign/FeignController.java
@@ -0,0 +1,21 @@
+package com.springcloud.feign;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/feign")
+public class FeignController {
+    
+    @Autowired
+    private HelloService helloService;
+
+    @GetMapping("/say/{name}")
+    public String sayHello(@PathVariable String name) {
+	return helloService.sayHello(name);
+    }
+
+}
